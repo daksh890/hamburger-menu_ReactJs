9c912b1430e0400fcf10b2efa86806cc1f7741ed
daksh890 <57205074+daksh890@users.noreply.github.com>
2021-10-01 08:25:36 +0530

    Hamburger tutorial completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
 
+import React, {useState, useRef} from 'react';
+import {useOnClickOutside} from './hooks';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyles } from './global';
+import { theme } from './theme';
+import{ Burger, Menu } from './Components';
 function App() {
+
+  const[open, setOpen] = useState(false);
+  const node = useRef();
+  useOnClickOutside(node, ()=>setOpen(false));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <>
+        <GlobalStyles />
+        <div ref={node}>
+          <Burger open={open} setOpen={setOpen}/>
+          <Menu open={open} setOpen={setOpen}/>
+        </div>
+        <div>
+          <h1>Hello. This is burger menu tutorial</h1>
+          <img src="https://image.flaticon.com/icons/svg/2016/2016012.svg" alt="burger icon" />
+          <small>Icon made by Freepik from www.flaticon.com</small>
+          <small>Project Tutuorial <a href="https://css-tricks.com/hamburger-menu-with-a-side-of-react-hooks-and-styled-components/"> <b>HERE</b></a></small>
+          <small><b>Thanks ~Daksh Lakhotiya</b></small>
+        </div>
+      </>
+    </ThemeProvider>
   );
 }
+
 
 export default App;
